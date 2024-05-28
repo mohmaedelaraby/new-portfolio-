ec3ad166bf446b1e59b783d27c584d6bc1b89f86
mohmaedelaraby <elarabym857@gmail.com>
2024-05-28 12:16:49 +0300

    add router

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import React from "react";
+import MainLayout from "./Layouts/MainLayout/MainLayout";
+import HomePage from "./Components/Home/HomePage";
+
+
+export const RootRoutes = () => {
+    const navigate = useNavigate();
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <MainLayout />
+                }
+            >
+                <Route path="/home" index element={<HomePage />} />
+                <Route path="/contact-us" index element={<HomePage />} />
+                <Route path="/about-me" index element={<HomePage />} />
+                <Route path="/resume" index element={<HomePage />} />
+                <Route path="/protofolio" index element={<HomePage />} />
+            </Route>
+        </Routes>
+    );
+};
