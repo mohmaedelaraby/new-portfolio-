cfeb4d5bab9f710dc0c93c79642d86ed181a8e1f
mohmaedelaraby <elarabym857@gmail.com>
2024-05-30 15:28:39 +0300

    add toe animation

--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './HomePage.css'
+import { ReactTyped } from 'react-typed'
 function HomePage() {
+  const right = `{" `
+  const left = ` "}`
   return (
     <div className='homepage'>
       <div className="homepage_container">
         <div className="my_name">
           <div className="name_text">// HI, I'M Mohamed EL-Araby, A...</div>
-          <div className="name_animation">Front End Engineer</div>
+          <div className="name_animation">
+            {right}
+          <ReactTyped showCursor startWhenVisible strings={["_Front End Engineer..."]} typeSpeed={100} backSpeed={100} loop className='name_animation_text'/>
+          {left}
+          </div>
         </div>
         <div className="bg_1">
 
